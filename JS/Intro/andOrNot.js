c0674080d1350da0ab97ec7adfeb32eb24709f40
brianoduor1-sudo <brian.oduor1@student.moringaschool.com>
2026-06-22 11:05:32 +0300

    m

---
+++ JS/Intro/andOrNot.js
@@ -0,0 +1,43 @@
+/*
+comparison operators
+comparison operators evaluate to only true or false
+*/
+
+/*
+And -> &&
+true table _. all posible combinations
+val1 and val2
+combinations are
+val1 && val2
+Val2&& val1
+-> for and all statements have to be true for
+ it to be true
+*/
+let val1=true
+let val2=true
+
+let result1=val1&&val2
+
+
+console.log(`val=${val1} val2=${val2} val1&&val2=${val1&&val2}`)
+
+let oluthe_age=30
+let oluthe_balance=80000
+
+//if oluthe is age is greater than 18 and has more than 30000 in his account
+
+//Method 1
+let is_greater_than18=oluthe_age>18
+console.log(`Is oluthe age greater than 18 ${is_greater_than18}`)
+let has_more_than30000=oluthe_balance>30000
+console.log(`oluthe has more than 30000 ${has_more_than30000}`)
+console.log(`oluthe age is greater than 18 
+    and has more than 30000 in his account 
+    ${is_greater_than18&&has_more_than30000}`)
+
+//oluthe_age>18&&oluthe_balance>30000
+//Method 2
+
+let simmplifed=oluthe_age>18&&oluthe_balance>30000
+console.log(`simplified ${simmplifed}`)
+console.log(`further simplification is ${oluthe_age>18&&oluthe_balance>30000}`)
