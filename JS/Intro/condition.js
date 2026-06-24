09b2bd1d5601eecaca8cf218043bc2673da1ce90
brianoduor1-sudo <brian.oduor1@student.moringaschool.com>
2026-06-24 10:07:18 +0300

    learnt about the if condition

---
+++ JS/Intro/condition.js
@@ -0,0 +1,24 @@
+/*
+conditions->
+control the flow of the code by making a decision
+*/
+const mylife="Learning condition"
+if(mylife){console.log("scenario 1 to run")}
+
+const mylife2=""
+if(mylife2){console.log("scenario 2")}
+
+if(true){console.log("sceranio 3 it run")}
+
+if(false){console.log("sceranio 4 it run")}
+
+let x=30<10
+if(x){console.log("scenario 5 it run")}
+
+let y=30>10
+
+if(30>10){console.log("scenario 6 it run")}
+
+if(50>10){console.log("scenario 7 it run")}
+
+if(50<10){console.log("scenario 8 it run")}
