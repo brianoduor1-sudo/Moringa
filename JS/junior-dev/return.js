b77159a44d04fa005c77325b13fb9e108deb1a3b
brianoduor1-sudo <brian.oduor1@student.moringaschool.com>
2026-06-30 10:46:03 +0300

    learnt about return

---
+++ JS/junior-dev/return.js
@@ -0,0 +1,16 @@
+/*
+ 1. exist code
+  2. return any required data
+*/
+function sayMyname(){
+    let first_name = "Brian" // statement 1
+    console.log(` First Name ${first_name}`); // statement 2
+    let second_name = " Oluoch" // statement 3
+    console.log(`Second Name ${second_name}`); // statement 4
+    let full_name= `${first_name} ${second_name}`; //statement 5
+    console.log(`Names are ${false}`); //statement 6
+}
+
+let sayMyNameValue = sayMyname();
+
+console.log(`Function returned ${sayMyname}`);
