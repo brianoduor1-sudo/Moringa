87bb7f70fe47af410ca27037f3379c2517f39629
brianoduor1-sudo <brian.oduor1@student.moringaschool.com>
2026-07-01 10:46:51 +0300

    types of functions and statements and expression

---
+++ JS/junior-dev/statementAndExpression.js
@@ -0,0 +1,20 @@
+/*
+Expression
+  A piece of code that produces a value
+  x>5 => true or false
+  10+11 => 21 value
+  circleArea(5) = expression function call
+   reference value -> n1
+
+*/
+let n1 = 23;  // statement declare value
+// statement created a function
+function sayMyname() {
+    console.log("Brian Oduor");
+    
+}
+
+console.log(n1); //23 n1=23//
+console.log(10+12); //10+12// 22
+console.log(n1 > 10); //n1.10 or true or false
+console.log(sayMyname()); // function call=> undefined
---
+++ JS/junior-dev/typesOfFunction.js
@@ -0,0 +1,26 @@
+/*
+ 1. Named Function:
+ 2.
+ Anonymous Function: <without a name>
+     -> a function can be assigned to variable
+     -> a variable can reference a function
+*/
+//function areaCircle(r) {
+  //  return 3.142*r*r; // area of a functiom
+// named function
+
+//
+let age = 23;
+let samsAge = age; // age= 23
+let ac = areaCircle
+
+//console.log(ac)
+
+
+function areaTriangle(b,h) {
+    return 0.5*b*h
+}
+let olu=areaTriangle
+
+let olu=function areaTriangle(b,h) {
+    return 0.5*b*h}
