c0e338ce5916cbef069f809ce870a55217abd54e
Brian Oduor <brian.oduor1@student.moringaschool.com>
2026-08-04 17:10:17 +0300

    Learnt about Forms

--- REACT/vite-project/src/StateAndEvents/Form.jsx
+++ REACT/vite-project/src/StateAndEvents/Form.jsx
@@ -1,65 +1,70 @@
-/*
-How to handle form input and events
-*/
-
+import { useState } from "react";
 function Form() {
-  // event when name changes
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
   const nameOnChange = (e) => {
-    console.log("NAME IS", e.target.value);
+    setName(e.target.value);
   };
-
-  //
-
-  const emailOnChange = (e) => {
-    console.log("Email IS", e.target.value);
-  };
-
-  //
-
-  const passwordOnChange = (e) => {
-    console.log("Password is", e.target.value);
-  };
-
-  //
-
   const onSubmit = () => {
     console.log("Submit button clicked");
+    console.log("name is", name);
+    console.log("email is", email);
+    console.log("password is", password);
   };
-
   return (
     <div>
+      {" "}
       <main>
+        {" "}
         <div>
-          <label>Name</label>
-        </div>
+          {" "}
+          <label>Name</label>{" "}
+        </div>{" "}
         <div>
-          <input onChange={nameOnChange} />
-        </div>
-      </main>
-
+          {" "}
+          <input value={name} onChange={nameOnChange} />{" "}
+        </div>{" "}
+      </main>{" "}
       <main>
+        {" "}
         <div>
-          <label>Email</label>
-        </div>
+          {" "}
+          <label>Email</label>{" "}
+        </div>{" "}
         <div>
-          <input onChange={emailOnChange} />
-        </div>
-      </main>
-
+          {" "}
+          <input
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+          />{" "}
+        </div>{" "}
+      </main>{" "}
       <main>
+        {" "}
         <div>
-          <label>Password</label>
-        </div>
+          {" "}
+          <label>Password</label>{" "}
+        </div>{" "}
         <div>
-          <input type="password" onChange={passwordOnChange} />
-        </div>
-      </main>
-
+          {" "}
+          <input
+            type="password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+          />{" "}
+        </div>{" "}
+      </main>{" "}
       <main>
-        <button onClick={onSubmit}>Submit</button>
-      </main>
+        {" "}
+        <button onClick={onSubmit}>Submit</button>{" "}
+      </main>{" "}
+      <ul>
+        {" "}
+        <li>Name: {name}</li> <li>Email: {email}</li>{" "}
+        <li>Password: {password}</li>{" "}
+      </ul>{" "}
     </div>
   );
 }
-
 export default Form;
--- REACT/vite-project/src/main.jsx
+++ REACT/vite-project/src/main.jsx
@@ -1,7 +1,8 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 
-import StateAndEvents from "./StateAndEvents/ClickMe.jsx";
+import StateAndEvents from "./StateAndEvents/Form2.jsx";
+
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     <StateAndEvents />
