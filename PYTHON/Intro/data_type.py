ef368bf01fab9ed3614c2f7e8b86a21b46dfb2c1
Brian Oduor <brioluoch@gmail.com>
2026-08-25 10:16:53 +0300

    Intro to Python

---
+++ PYTHON/Intro/data_type.py
@@ -0,0 +1,22 @@
+# Example Variables
+x = 5  # integer variable
+y = 3.14  # float variable
+z = "Hello, World!"  # string variable
+a = True  # boolean variable    
+b = [1, 2, 3, 4, 5]  # list variable
+c = (6, 7, 8, 9, 10)  # tuple variable
+d = {"name": "John", "age": 30}  # dictionary variable
+e = {1, 2, 3, 4, 5}  # set variable
+
+# Dictionary <Object:js>
+# for dctionary use Bracket Notation
+
+# Determining the data type of each variable
+print("X is x:", "Its type is", type(x))
+print("Y is y:", "Its type is", type(y))
+print("Z is z:", "Its type is", type(z))
+print("A is a:", "Its type is", type(a))
+print("B is b:", "Its type is", type(b))
+print("C is c:", "Its type is", type(c))
+print("D is d:", "Its type is", type(d))
+print("E is e:", "Its type is", type(e))
---
+++ PYTHON/Intro/start.py
@@ -0,0 +1,9 @@
+# is used to write comments in python
+#console.log = print
+#let Key = "value"  # variable declaration
+x=34
+print("X is=", x)  # print the value of x
+y=20
+print("Y is=", y)  # print the value of y
+z=x+y
+print("Z is=", z)  # print the value of z
