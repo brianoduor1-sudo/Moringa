698fe23e565cdde1854115f133c6ed0146ff17d6
Brian Oduor <brioluoch@gmail.com>
2026-08-26 10:31:02 +0300

    Learnt about Functions and Conditions

---
+++ PYTHON/Intro/function.py
@@ -0,0 +1,19 @@
+# Function to calculate the area of a rectangle
+def calculate_area(length, width):
+    return length * width
+
+# Function to check if a number is even
+def is_even(number):
+    return number % 2 == 0
+
+# Function to print a message
+def print_message(message):
+    print(message)
+def area_of_circle(radius):
+    pi = 3.14159
+    return pi * radius * radius
+print(area_of_circle(20))
+
+new_f=area_of_circle
+
+new_f(20)
